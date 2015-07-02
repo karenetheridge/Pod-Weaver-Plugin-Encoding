2d1b1e77116ad0fc58e62dbf5f0b01f31530502a
Karen Etheridge <ether@cpan.org>
2015-07-01 18:13:07 -0700

    increment $VERSION after 0.03 release

--- lib/Pod/Weaver/Plugin/Encoding.pm
+++ lib/Pod/Weaver/Plugin/Encoding.pm
@@ -1,7 +1,7 @@
 package Pod::Weaver::Plugin::Encoding;
 # ABSTRACT: (DEPRECATED) Add an encoding command to your POD
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 use Moose;
 use List::Util 1.33 'any';
